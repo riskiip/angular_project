3fcc24603652d89ad4fd0a5f2c9908fb3e37a25d
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2024-02-13 22:29:52 +0700

    init project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
